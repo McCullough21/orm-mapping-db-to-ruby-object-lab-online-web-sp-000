9160886003fc63e410d50e4a21ea9f1832421bec
Michael McCullough <michael.mccullough21@gmail.com>
2019-08-28 20:40:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,10 +86,10 @@ def self.first_X_students_in_grade_10(x)
   WHERE grade = 10
   SQL
   count = 1
+  until count = x do
   DB[:conn].execute(sql, x).map do |row|
     self.new_from_db(row)
     count += 1
-    until count = x
     end
   end
 end
